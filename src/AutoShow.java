43b3ef8db2ac50868e2e7d544423db1815a289c0
Soul <top.secret.work@yandex.ru>
2022-03-04 17:40:25 +0300

    update code

--- src/AutoShow.java
+++ src/AutoShow.java
@@ -1,27 +1,40 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class AutoShow{
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
     int currentAuto = 0;
 
-    public synchronized void buyCar() {
+    public void buyCar() {
+        lock.lock();
         try {
             Thread.sleep(1000);
             String buyer = Thread.currentThread().getName();
             System.out.println(buyer + " зашел в автосалон");
             while (currentAuto <= 0) {
                 System.out.println("Машин нет");
-                wait();
+                condition.await();
             }
             Thread.sleep(1000);
             currentAuto--;
             System.out.println(buyer + " прикупил машину");
-        } catch (InterruptedException ignored) { }
+        } catch (InterruptedException ignored) { } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized void getCar() {
+    public void getCar() {
+        lock.lock();
         try {
             Thread.sleep(1000);
             System.out.println("Производитель " + Thread.currentThread().getName() + " доставил машину в автосалон");
             currentAuto += 1;
-            notify();
-        } catch (InterruptedException ignored) { }
+            condition.signal();
+        } catch (InterruptedException ignored) { } finally {
+            lock.unlock();
+        }
     }
 }
